2bbd4525ea04ae29cdd78050181d815b969c9ea4
raghavendram39-netizen <raghavendram39@gmail.com>
2026-07-16 22:30:44 +0530

    live-market-api.js

--- live-market-api.js
+++ live-market-api.js
@@ -1,18 +1,25 @@
-const API_URL = "https://api.data.gov.in/resource/9ef84268-d588-465a-a308-a864a43d0070?api-key=YOUR_API_KEY&format=json&limit=1000";
-async function loadLiveMarket() {
+const API_URL = "https://api.data.gov.in/resource/9ef84268-d588-465a-a308-a864a43d0070?api-key=579b464db66ec23bdd0000015f77b57d96134b6762464b38c1b86fda&format=json&limit=100";
+function displayLiveMarket(records) {
 
-    try {
+    const container = document.getElementById("marketContainer");
 
-        const response = await fetch(API_URL);
+    container.innerHTML = "";
 
-        const data = await response.json();
+    records.forEach(record => {
 
-        displayLiveMarket(data.records);
+        container.innerHTML += `
+            <div class="market-card">
+                <h2>🌾 ${record.commodity}</h2>
+                <p><strong>State:</strong> ${record.state}</p>
+                <p><strong>Market:</strong> ${record.market}</p>
+                <p><strong>Min Price:</strong> ₹${record.min_price}</p>
+                <p><strong>Max Price:</strong> ₹${record.max_price}</p>
+                <p><strong>Modal Price:</strong> ₹${record.modal_price}</p>
+            </div>
+        `;
 
-    } catch (error) {
-
-        console.error("Error loading market data:", error);
-
-    }
+    });
 
 }
+
+window.onload = loadLiveMarket;
